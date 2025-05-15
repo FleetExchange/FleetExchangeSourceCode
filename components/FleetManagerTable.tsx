135b1fece8e8d75679d1328b5e67434b517e2480
Wietske User 2 <wietskeuser2@Wietskes-MacBook-Pro.local>
2025-05-15 12:46:50 +0200

    Fleet Manager Filter Added

--- components/FleetManagerTable.tsx
+++ components/FleetManagerTable.tsx
@@ -5,46 +5,55 @@ import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import { useEffect, useState } from "react";
 import { CiMenuKebab } from "react-icons/ci";
+import FleetManagerTableFilter from "./FleetManagerTableFilter";
 
 type SortOption = "registration" | "payload" | "length" | "width" | "height";
 
 const FleetManagerTable = () => {
+  // Get the logged in user identity
   const { user } = useUser();
-  const userId = user?.id ?? ""; // This is the Clerk user ID
+  // This is the Clerk user ID
+  const userId = user?.id ?? "";
 
   // Get all fleets that belong to user
   const userFleets = useQuery(api.fleet.getFleetForCurrentUser, {
     userId: userId,
   });
+  // Set the current selected fleet from selector
   const [userFleet, setUserFleet] = useState<string | undefined>(undefined);
 
   // Handle when the selected fleet changes
   const handleFleetChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setUserFleet(event.target.value);
   };
-
   useEffect(() => {
     if (userFleets && userFleets.length > 0) {
       setUserFleet(userFleets[0]._id);
     }
   }, [userFleets]);
 
+  // Stores which sorting is choses
   const [sortBy, setSortBy] = useState<
     "registration" | "payload" | "length" | "width" | "height"
   >("registration");
 
+  // Stores the terms to filter by
   const [filterTerm, setFilterTerm] = useState<{
+    make: string;
+    year: string;
     truckType: string;
-    width: string;
-    length: string;
-    height: string;
-    payload: string;
+    width: number;
+    length: number;
+    height: number;
+    payload: number;
   }>({
+    make: "",
+    year: "",
     truckType: "",
-    width: "",
-    length: "",
-    height: "",
-    payload: "",
+    width: 0,
+    length: 0,
+    height: 0,
+    payload: 0,
   });
 
   // Queries to fetch all the truck ID's belonging to selected user fleets
@@ -52,6 +61,7 @@ const FleetManagerTable = () => {
     api.fleet.getTrucksForFleet,
     userFleet ? { fleetId: userFleet as Id<"fleet"> } : "skip"
   );
+
   // Get all trucks by ID
   const getTrucks = useQuery(
     api.truck.getTruckByIdArray,
@@ -59,8 +69,22 @@ const FleetManagerTable = () => {
   );
   let trucks = [...(getTrucks ?? [])]; // Safe copy, never undefined
 
+  // Filtering the trucks
+  const filteredTrucks = trucks.filter((truck) => {
+    return (
+      (!filterTerm.make || truck.make === filterTerm.make) &&
+      (!filterTerm.year || truck.year === filterTerm.year) &&
+      (!filterTerm.truckType || truck.truckType === filterTerm.truckType) &&
+      (filterTerm.width == 0 || truck.width === filterTerm.width) &&
+      (filterTerm.length == 0 || truck.length === filterTerm.length) &&
+      (filterTerm.height == 0 || truck.height === filterTerm.height) &&
+      (filterTerm.payload == 0 || truck.maxLoadCapacity === filterTerm.payload)
+    );
+  });
+
+  // Sorting methods
   if (sortBy === "registration") {
-    trucks.sort((a, b) =>
+    filteredTrucks.sort((a, b) =>
       a.registration.localeCompare(b.registration, undefined, {
         numeric: true,
         sensitivity: "base",
@@ -68,18 +92,19 @@ const FleetManagerTable = () => {
     );
   }
   if (sortBy === "payload") {
-    trucks.sort((a, b) => a.maxLoadCapacity - b.maxLoadCapacity);
+    filteredTrucks.sort((a, b) => a.maxLoadCapacity - b.maxLoadCapacity);
   }
   if (sortBy === "length") {
-    trucks.sort((a, b) => a.length - b.length);
+    filteredTrucks.sort((a, b) => a.length - b.length);
   }
   if (sortBy === "width") {
-    trucks.sort((a, b) => a.width - b.width);
+    filteredTrucks.sort((a, b) => a.width - b.width);
   }
   if (sortBy === "height") {
-    trucks.sort((a, b) => a.height - b.height);
+    filteredTrucks.sort((a, b) => a.height - b.height);
   }
 
+  // If the user has no fleets, a message is shown
   if (!userFleets) {
     return <p>Loading fleets...</p>; // or a spinner
   }
@@ -90,7 +115,9 @@ const FleetManagerTable = () => {
         {/** Action bar */}
         <div className="felx-row flex justify-between gap-2 bg-base-100 border-1 border-base-300 rounded-t-xl items-center px-5 py-2">
           <div className="flex flex-row justify-start gap-4">
+            {/* Search Bar */}
             <input type="search" className="grow" placeholder="Search" />
+            {/* Fleet selector */}
             <select
               defaultValue={userFleet}
               onChange={handleFleetChange}
@@ -102,6 +129,7 @@ const FleetManagerTable = () => {
                 </option>
               ))}
             </select>
+            {/* Sorting Selector */}
             <select
               className="select"
               value={sortBy}
@@ -122,9 +150,15 @@ const FleetManagerTable = () => {
               <option value="width">Sort By Width</option>
               <option value="height">Sort By Height</option>
             </select>
-            <button className="btn">Insert Filter</button>
+            {/* Filter Button */}
+            <FleetManagerTableFilter
+              onFilter={(filterTerm) => {
+                setFilterTerm(filterTerm);
+              }}
+            />
           </div>
 
+          {/* Action Button */}
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-primary m-1">
               Actions
@@ -157,15 +191,16 @@ const FleetManagerTable = () => {
                   <th>Make</th>
                   <th>Model</th>
                   <th>Year</th>
-                  <th>Payload Capacity</th>
-                  <th>Length</th>
-                  <th>Width</th>
-                  <th>Height</th>
+                  <th>Payload Capacity (kg)</th>
+                  <th>Length (m) </th>
+                  <th>Width (m) </th>
+                  <th>Height (m) </th>
                   <th></th>
                 </tr>
               </thead>
+              {/* Table Body */}
               <tbody>
-                {trucks.map((truck, index) => (
+                {filteredTrucks.map((truck, index) => (
                   <tr key={truck.registration} className="bg-base-100">
                     <th>{index + 1}</th>
                     <td>{truck.registration}</td>
--- components/FleetManagerTableFilter.tsx
+++ components/FleetManagerTableFilter.tsx
@@ -1,41 +1,35 @@
 "use client";
 
 import { TRUCK_TYPES } from "@/shared/truckTypes";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { IoFilter } from "react-icons/io5";
 
 const FleetManagerTableFilter = ({
   onFilter,
 }: {
   onFilter: (filters: {
-    depDate: string;
-    depTime: string;
-    arrDate: string;
-    arrTime: string;
     truckType: string;
-    width: string;
-    length: string;
-    height: string;
-    payload: string;
+    make: string;
+    year: string;
+    width: number;
+    length: number;
+    height: number;
+    payload: number;
   }) => void;
 }) => {
-  const [depDate, setDepDate] = useState("");
-  const [depTime, setDepTime] = useState("");
-  const [arrDate, setArrDate] = useState("");
-  const [arrTime, setArrTime] = useState("");
+  const [year, setYear] = useState("");
+  const [make, setMake] = useState("");
   const [truckType, setType] = useState("");
-  const [width, setWidth] = useState("");
-  const [length, setlength] = useState("");
-  const [height, setHeight] = useState("");
-  const [payload, setPayload] = useState("");
+  const [width, setWidth] = useState<number>(0);
+  const [length, setlength] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+  const [payload, setPayload] = useState<number>(0);
 
   const applyFilters = (e: React.FormEvent) => {
     e.preventDefault();
     onFilter({
-      depDate,
-      depTime,
-      arrDate,
-      arrTime,
+      year,
+      make,
       truckType,
       width,
       length,
@@ -47,20 +41,16 @@ const FleetManagerTableFilter = ({
 
   const resetFilters = (e: React.FormEvent) => {
     e.preventDefault();
-    setDepDate("");
-    setDepTime("");
-    setArrDate("");
-    setArrTime("");
-    setWidth("");
-    setlength("");
-    setHeight("");
-    setPayload("");
+    setMake("");
+    setYear("");
+    setWidth(0);
+    setlength(0);
+    setHeight(0);
+    setPayload(0);
     setType("");
     onFilter({
-      depDate,
-      depTime,
-      arrDate,
-      arrTime,
+      make,
+      year,
       truckType,
       width,
       length,
@@ -70,7 +60,7 @@ const FleetManagerTableFilter = ({
   };
   const openModal = () => {
     const modal = document.getElementById(
-      "my_modal"
+      "FleetManagerFilterModal"
     ) as HTMLDialogElement | null;
     if (modal) {
       modal.showModal();
@@ -88,59 +78,10 @@ const FleetManagerTableFilter = ({
         <IoFilter /> Filters
       </button>
 
-      <dialog id="my_modal" className="modal">
+      <dialog id="FleetManagerFilterModal" className="modal">
         <div className="modal-box w-full max-w-[500px]">
           <div>
             <h1 className="font-bold mb-4">Filters</h1>
-            <h1 className="font-bold">Specific Departure Date & Time</h1>
-            <div className="flex flex-row gap-2">
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">Date</legend>
-                <input
-                  type="date"
-                  className="input focus:outline-none focus:ring-0"
-                  placeholder="Type here"
-                  value={depDate}
-                  onChange={(e) => setDepDate(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">Time</legend>
-                <input
-                  type="time"
-                  className="input focus:outline-none focus:ring-0"
-                  placeholder="Type here"
-                  value={depTime}
-                  onChange={(e) => setDepTime(e.target.value)}
-                />
-              </fieldset>
-            </div>
-          </div>
-          <hr className="border-t border-base-200 my-0 mt-4" />
-          <div className="mt-4">
-            <h1 className="font-bold">Specific Arrival Date & Time</h1>
-            <div className="flex flex-row gap-2">
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">Date</legend>
-                <input
-                  type="date"
-                  className="input focus:outline-none focus:ring-0"
-                  placeholder="Type here"
-                  value={arrDate}
-                  onChange={(e) => setArrDate(e.target.value)}
-                />
-              </fieldset>
-              <fieldset className="fieldset">
-                <legend className="fieldset-legend">Time</legend>
-                <input
-                  type="time"
-                  className="input focus:outline-none focus:ring-0"
-                  placeholder="Type here"
-                  value={arrTime}
-                  onChange={(e) => setArrTime(e.target.value)}
-                />
-              </fieldset>
-            </div>
           </div>
           <hr className="border-t border-base-200 my-0 mt-4" />
           <div className="mt-4">
@@ -164,6 +105,34 @@ const FleetManagerTableFilter = ({
               </fieldset>
             </div>
           </div>
+
+          <hr className="border-t border-base-200 my-0 mt-4" />
+          <div className="mt-4">
+            <h1 className="font-bold">Vehcile Information</h1>
+            <div className="flex flex-row gap-2">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Vehcile Make</legend>
+                <input
+                  type="text"
+                  className="input focus:outline-none focus:ring-0"
+                  placeholder="Type here"
+                  value={make}
+                  onChange={(e) => setMake(e.target.value)}
+                />
+              </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend">Year of Model</legend>
+                <input
+                  type="number"
+                  className="input focus:outline-none focus:ring-0"
+                  placeholder="Type here"
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                />
+              </fieldset>
+            </div>
+          </div>
+
           <hr className="border-t border-base-200 my-0 mt-4" />
           <div className="mt-4">
             <h1 className="font-bold">Dimensions & Capacity</h1>
@@ -172,10 +141,13 @@ const FleetManagerTableFilter = ({
                 <legend className="fieldset-legend">Width</legend>
                 <input
                   type="number"
+                  min="0"
                   className="input focus:outline-none focus:ring-0"
                   placeholder="Type here"
                   value={width}
-                  onChange={(e) => setWidth(e.target.value)}
+                  onChange={(e) =>
+                    setWidth(e.target.value === "" ? 0 : Number(e.target.value))
+                  }
                 />
                 <p className="label">(In Meter)</p>
               </fieldset>
@@ -183,10 +155,15 @@ const FleetManagerTableFilter = ({
                 <legend className="fieldset-legend">Length</legend>
                 <input
                   type="number"
+                  min="0"
                   className="input focus:outline-none focus:ring-0"
                   placeholder="Type here"
                   value={length}
-                  onChange={(e) => setlength(e.target.value)}
+                  onChange={(e) =>
+                    setlength(
+                      e.target.value === "" ? 0 : Number(e.target.value)
+                    )
+                  }
                 />
                 <p className="label">(In Meter)</p>
               </fieldset>
@@ -194,10 +171,15 @@ const FleetManagerTableFilter = ({
                 <legend className="fieldset-legend">Height</legend>
                 <input
                   type="number"
+                  min="0"
                   className="input focus:outline-none focus:ring-0"
                   placeholder="Type here"
                   value={height}
-                  onChange={(e) => setHeight(e.target.value)}
+                  onChange={(e) =>
+                    setHeight(
+                      e.target.value === "" ? 0 : Number(e.target.value)
+                    )
+                  }
                 />
                 <p className="label">(In Meter)</p>
               </fieldset>
@@ -205,10 +187,15 @@ const FleetManagerTableFilter = ({
                 <legend className="fieldset-legend">Payload Capacity</legend>
                 <input
                   type="number"
+                  min="0"
                   className="input focus:outline-none focus:ring-0"
                   placeholder="Type here"
                   value={payload}
-                  onChange={(e) => setPayload(e.target.value)}
+                  onChange={(e) =>
+                    setPayload(
+                      e.target.value === "" ? 0 : Number(e.target.value)
+                    )
+                  }
                 />
                 <p className="label">(In Kg)</p>
               </fieldset>
