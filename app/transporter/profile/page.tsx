571189fda832629ff9110dd658b22fe8ac4bc0fb
Wietske User 2 <wietskeuser2@Wietskes-MacBook-Pro.local>
2025-07-22 17:54:13 +0200

    transporter profile layout fixed

--- app/transporter/profile/page.tsx
+++ app/transporter/profile/page.tsx
@@ -15,9 +15,17 @@ const Page = () => {
   if (!currentUser) return <div>Loading...</div>;
 
   return (
-    <div>
-      <TransporterProfileInfo transporterId={currentUser._id} />
-      <TransporterProfileFiles transporterId={currentUser._id} />
+    <div className="min-h-screen pl-24 p-6 bg-base-200 flex flex-col items-center">
+      <div className="w-full max-w-6xl flex flex-col md:flex-row gap-8">
+        {/* Profile Info - take 2/3 width on desktop */}
+        <div className="flex-1 min-w-0">
+          <TransporterProfileInfo transporterId={currentUser._id} />
+        </div>
+        {/* Business Documents - take 1/3 width on desktop */}
+        <div className="w-full md:w-[340px] flex-shrink-0">
+          <TransporterProfileFiles transporterId={currentUser._id} />
+        </div>
+      </div>
     </div>
   );
 };
