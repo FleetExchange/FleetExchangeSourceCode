50a3dd4135ec935ca3f6cae7f9fd70ca09c100a0
Wietske User 2 <wietskeuser2@Wietskes-MacBook-Pro.local>
2025-09-29 09:38:48 +0200

    Bank resolve api params updated

--- app/api/paystack/create-recipient/route.ts
+++ app/api/paystack/create-recipient/route.ts
@@ -6,24 +6,21 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const { name, accountNumber, bankCode, email } = body;
 
-    const response = await fetch(
-      "https://api.paystack.co/za/transferrecipient",
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          type: "basa",
-          name: name,
-          account_number: accountNumber,
-          bank_code: bankCode,
-          currency: "ZAR",
-          email: email,
-        }),
-      }
-    );
+    const response = await fetch("https://api.paystack.co/transferrecipient", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "basa",
+        name: name,
+        account_number: accountNumber,
+        bank_code: bankCode,
+        currency: "ZAR",
+        email: email,
+      }),
+    });
 
     const data = await response.json();
     return NextResponse.json(data);
